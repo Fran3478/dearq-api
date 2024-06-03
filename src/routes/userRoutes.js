3077e774cd27d86e92d3ea514b434eb7d87c8912
Francisco Hernandez <franciscohv95@gmail.com>
2024-06-03 20:33:03 -0300

    relocate test root

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,5 +5,5 @@ const route = Router()
 
 export default (app) => {
     app.use("/user", route)
-    route.post("/test", validateToken, (req, res) => {res.send("hello world")})
+    route.post("/recover-password", validateToken, )
 }
