f415e86ae24321bfac933c3d1163f55850812999
Francisco Hernandez <franciscohv95@gmail.com>
2024-09-02 20:26:38 -0300

    improve middleware for user check

--- src/middleware/checkUser.js
+++ src/middleware/checkUser.js
@@ -1,14 +1,14 @@
 import { PermissionError, UserNotFoundError } from "../errors/index.js"
-import {find} from "../repositories/user/index.js"
+import { find } from "../repositories/user/index.js"
 
 export default async (req, res, next) => {
     try {
-        if(req._user._role === "admin"){
+        if(req._user) {
             const user = await find({username: req._user._id, type: "id"})
             if(!user) throw new UserNotFoundError("No existe el usuario")
-            if(user.role === "admin") return next()
+            if(user.role === "admin" || user.role === "user") return next()
         }
-        throw new PermissionError("No posee permiso para la acción solicitada")
+    throw new PermissionError("No posee permiso para la acción solicitada")
     } catch (err) {
         next(err)
     }
