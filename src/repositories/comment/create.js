d156490e149ccf1ce0fa546d0537499f792c5b00
Francisco Hernandez <franciscohv95@gmail.com>
2024-09-03 17:56:06 -0300

    create feature fix to work with comments and replies

--- src/repositories/comment/create.js
+++ src/repositories/comment/create.js
@@ -1,10 +1,10 @@
 import { CommentCreationError } from "../../errors/index.js"
 import { Comment } from "../../models/index.js"
 
-export default async ({content, userId, transaction, postId}) => {
+export default async ({content, userId, transaction, postId, parentId = null}) => {
     try {
         const currentDate = new Date()
-        const newComment = await Comment.create({content, created_date: currentDate, userId, postId}, {transaction})
+        const newComment = await Comment.create({content, created_date: currentDate, userId, postId, parentId}, {transaction})
         return newComment
     } catch (err) {
         throw new CommentCreationError("No se pudo registrar el comentario")
--- src/services/comment/newComment.js
+++ src/services/comment/newComment.js
@@ -2,10 +2,10 @@ import { CommentCreationError, CommentError } from "../../errors/index.js"
 import { sequelize } from "../../models/index.js"
 import { create } from "../../repositories/comment/index.js"
 
-export default async ({content, userId, postId}) => {
+export default async ({content, userId, postId, parentId = null}) => {
     const transaction = await sequelize.transaction()
     try {
-        const newComment = await create({content, transaction, userId, postId})
+        const newComment = await create({content, transaction, userId, postId, parentId})
         return newComment
     } catch (err) {
         if(err instanceof CommentCreationError) throw new CommentError(err.message, err.orig_error)
