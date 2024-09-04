a6708b4ada67c5b85d7e25491cdbfa90d4a0483e
Francisco Hernandez <franciscohv95@gmail.com>
2024-09-04 19:38:15 -0300

    controller index update

---
+++ src/controllers/comment/index.js
@@ -0,0 +1,15 @@
+import newComment from "./newComment.js"
+import newReplay from "./newReplay.js"
+import editComment from "./editComment.js"
+import likeComment from "./likeComment.js"
+import deleteComment from "./deleteComment.js"
+import blockComment from "./blockComment.js"
+
+export {
+    newComment,
+    newReplay,
+    editComment,
+    likeComment,
+    deleteComment,
+    blockComment
+}
