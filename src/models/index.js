d4c87e4d301a2bf4a5b39a90d603bf7667f443e7
Francisco Hernandez <franciscohv95@gmail.com>
2024-09-04 19:42:38 -0300

    update commentLike relation with user and comment

--- src/models/index.js
+++ src/models/index.js
@@ -5,6 +5,7 @@ import Post from "./Post.js"
 import PostContent from "./PostContent.js"
 import PostView from "./PostView.js"
 import User from "./User.js"
+import CommentLike from "./CommentLike.js"
 
 User.hasMany(Post, {foreignKey: "authorId", as: "authoredPosts", onDelete: "SET NULL"})
 User.hasMany(Post, {foreignKey: "editorId", as: "editedPosts", onDelete: "SET NULL"})
@@ -28,6 +29,12 @@ Comment.hasMany(Comment, {foreignKey: "parentId", as: "replies", onDelete: "CASC
 Comment.belongsTo(Comment, {foreignKey: "parentId", as: "parent", onDelete: "CASCADE"})
 Comment.belongsTo(User, {foreignKey: "userId", as: "author", onDelete: "CASCADE"})
 User.hasMany(Comment, {foreignKey: "userId", as: "authoredComments", onDelete: "CASCADE"})
+User.belongsToMany(Comment, {through: "CommentLike", foreignKey: "userid", as: "likedComments", onDelete: "CASCADE"})
+Comment.belongsToMany(User, {through: "CommentLike", foreignKey: "commentId", as: "usersWhoLiked", onDelete: "CASCADE"})
+User.hasMany(CommentLike, {foreignKey: "userId", as: "commentLikes", onDelete: "CASCADE"})
+Comment.hasMany(CommentLike, {foreignKey: "commentId", as: "likes", onDelete: "CASCADE"})
+CommentLike.belongsTo(User, {foreignKey: "userId", as: "user", onDelete: "CASCADE"})
+CommentLike.belongsTo(Comment, {foreignKey: "commentId", as: "comment", onDelete: "CASCADE"})
 
 export {
     sequelize,
