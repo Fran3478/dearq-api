2434d32c764ca46811eabb78621ed00c1d25db89
Francisco Hernandez <franciscohv95@gmail.com>
2024-09-04 20:21:49 -0300

    add increment like repository

---
+++ src/repositories/comment/incremetLike.js
@@ -0,0 +1,11 @@
+import { CommentLikeIncrementError } from "../../errors/index.js"
+import { CommentLike } from "../../models/index.js"
+
+export default async ({commentId, transaction}) => {
+    try {
+        await CommentLike.increment("likeCount", {by: 1, where: {id: commentid}, transaction})
+        return true
+    } catch (err) {
+        throw new CommentLikeIncrementError("Error al incrementar likes", err)
+    }
+}
