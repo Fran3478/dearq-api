b0e87d4fc2ad615f618a9d6f268cfa36681210b0
Francisco Hernandez <franciscohv95@gmail.com>
2024-09-05 15:53:04 -0300

    fix increment like repository

--- src/repositories/comment/incremetLike.js
+++ src/repositories/comment/incremetLike.js
@@ -3,7 +3,7 @@ import { CommentLike } from "../../models/index.js"
 
 export default async ({commentId, transaction}) => {
     try {
-        await CommentLike.increment("likesCount", {by: 1, where: {id: commentid}, transaction})
+        await CommentLike.increment("likesCount", {by: 1, where: {id: commentId}, transaction})
         return true
     } catch (err) {
         throw new CommentLikeIncrementError("Error al incrementar likes", err)
