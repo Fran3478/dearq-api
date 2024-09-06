80d20c22de902190ae13c1ad6c72e2d89e27f165
Francisco Hernandez <franciscohv95@gmail.com>
2024-09-06 18:37:31 -0300

    fix model error

--- src/repositories/comment/incremetLike.js
+++ src/repositories/comment/incremetLike.js
@@ -1,11 +1,11 @@
-import { CommentLikeIncrementError } from "../../errors/index.js"
-import { CommentLike } from "../../models/index.js"
+import { CommentLikeCountError } from "../../errors/index.js"
+import { Comment } from "../../models/index.js"
 
 export default async ({commentId, transaction}) => {
     try {
-        await CommentLike.increment("likesCount", {by: 1, where: {id: commentId}, transaction})
+        await Comment.increment("likesCount", {by: 1, where: {id: commentId}, transaction})
         return true
     } catch (err) {
-        throw new CommentLikeIncrementError("Error al incrementar likes", err)
+        throw new CommentLikeCountError("Error al incrementar likes", err)
     }
 }
