08fd03ab1c3d74ed5f30ce5eb94b46f219e86e7f
Francisco Hernandez <franciscohv95@gmail.com>
2024-09-06 19:48:03 -0300

    fix like and unlike response and unlike check exist param

--- src/controllers/comment/likeComment.js
+++ src/controllers/comment/likeComment.js
@@ -4,7 +4,7 @@ export default async (req, res, next) => {
     try {
         await checkExist({commentId: req.params.commentId})
         await likeComment({userId: req._user._id, commentId: req.params.commentId})
-        return res.status(200).json({message: 'Comentario marcado como "Me gusta"' })
+        return res.status(200).json({message: "Comentario marcado como me gusta" })
     } catch (err) {
         next(err)
     }
