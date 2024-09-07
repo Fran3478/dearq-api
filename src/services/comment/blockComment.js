c4c4d62f43230ed7c440b13e5deabcad5fb40a47
Francisco Hernandez <franciscohv95@gmail.com>
2024-09-06 21:04:45 -0300

    add blocked and deleted checkers

--- src/services/comment/blockComment.js
+++ src/services/comment/blockComment.js
@@ -1,20 +1,24 @@
-import { CommentError, CommentNotFoundError, CommentSearchError, CommentUpdateError } from "../../errors/index.js"
+import { CommentBlockError, CommentError, CommentNotFoundError, CommentSearchError, CommentUpdateError } from "../../errors/index.js"
 import { sequelize } from "../../models/index.js"
-import { update } from "../../repositories/comment/index.js"
+import { findById, update } from "../../repositories/comment/index.js"
 
 export default async ({commentId, reason}) => {
     const transaction = await sequelize.transaction()
     try {
         const commentToBlock = await findById({id: commentId})
+        if(commentToBlock.blocked) throw new CommentBlockError("El comentario ya se encuentra bloqueado")
+        if(commentToBlock.deleted) throw new CommentBlockError("El comentario que intenta bloquear ya fue eliminado")
         const updates = {
             blocked: true,
             blocked_date: new Date(),
             block_reason: reason
         }
         await update({comment: commentToBlock, updates, transaction})
+        await transaction.commit()
         return true
     } catch (err) {
-        if(err instanceof CommentNotFoundError) throw err
+        await transaction.rollback()
+        if(err instanceof CommentNotFoundError || err instanceof CommentBlockError) throw err
         if(err instanceof CommentSearchError ||  err instanceof CommentUpdateError) throw new CommentError(err.message, err.orig_error)
         throw new CommentError("Hubo un error al intentar bloquear el comentario", err)
     }
--- src/services/comment/deleteComment.js
+++ src/services/comment/deleteComment.js
@@ -1,4 +1,4 @@
-import { CommentError, CommentNotFoundError, CommentSearchError, CommentUpdateError, PermissionError } from "../../errors/index.js"
+import { CommentDeleteError, CommentError, CommentNotFoundError, CommentSearchError, CommentUpdateError, PermissionError } from "../../errors/index.js"
 import { sequelize } from "../../models/index.js"
 import { findById, update } from "../../repositories/comment/index.js"
 
@@ -8,6 +8,8 @@ export default async ({commentId, userId}) => {
         const commentToDelete = await findById({id: commentId})
 
         if(commentToDelete.userId !== userId) throw new PermissionError("El usuario no posee permiso para editar el comentario")
+        if(commentToDelete.deleted) throw new CommentDeleteError("El comentario ya se encuentra eliminado")
+        if(commentToDelete.blocked) throw new CommentDeleteError("El comentario que intenta eliminar esta bloqueado")
         const updates = {
             deleted: true,
             deleted_date: new Date()
@@ -17,7 +19,7 @@ export default async ({commentId, userId}) => {
         return true
     } catch (err) {
         await transaction.rollback()
-        if(err instanceof PermissionError || err instanceof CommentNotFoundError) throw err
+        if(err instanceof PermissionError || err instanceof CommentNotFoundError || err instanceof CommentDeleteError) throw err
         if(err instanceof CommentSearchError ||  err instanceof CommentUpdateError) throw new CommentError(err.message, err.orig_error)
         throw new CommentError("Hubo un error al intentar eliminar el comentario", err)
     }
