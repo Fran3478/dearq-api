ec38a271d86f7c3c80dd8c4bdeb9fab842093646
Francisco Hernandez <franciscohv95@gmail.com>
2024-09-09 23:44:06 -0300

    improve get comments service

--- src/services/comment/getComments.js
+++ src/services/comment/getComments.js
@@ -1,16 +1,18 @@
-import { CommentSearchError } from "../../errors/index.js"
+import { CommentSearchError, PostNotFoundError, PostSearchError } from "../../errors/index.js"
 import { findAll } from "../../repositories/comment/index.js"
+import { checkExist } from "../post/index.js"
 
 export default async ({page, postId, parentId = null}) => {
     try {
+        await checkExist({postId, published: true})
         const pageSize = 15
         const searchParameters = {
             pageSize,
             page,
             where: {
                 postId,
-                parentId
-            }
+            },
+            parentId
         }
         const results = await findAll({searchParameters})
         const totalPages = Math.ceil(results.totalComments / pageSize)
@@ -23,6 +25,7 @@ export default async ({page, postId, parentId = null}) => {
         results.totalPages = totalPages
         return results
     } catch (err) {
+        if(err instanceof PostNotFoundError || err instanceof PostSearchError) throw new PostNotFoundError("El post no existe o no está publicado actualmente", err)
         if(err instanceof CommentSearchError) throw err
         throw new CommentSearchError("Hubo un error", err)
     }
